453164d609aabf6da640064cdd3cae7e81a48e29
Michael Reda <eng.michaelreda@gmail.com>
2022-09-24 15:57:14 +0200

    repairing maya auto starting

--- src/dcc/maya/hooks/userSetup.py
+++ src/dcc/maya/hooks/userSetup.py
@@ -1,22 +1,39 @@
-# -*- coding: utf-8 -*-
+# >>>>>>>>>
+# Djed Tools
+# Add self
+import pathlib
+
 import maya.cmds as cmds
 
-cmds.evalDeferred("initPlugin()")
+
+def open_commandPort():
+    if not cmds.commandPort(":4436", query=True):
+        cmds.commandPort(name=":4436", sourceType="python")
 
 
-def initPlugin():
+def init_djed():
     import traceback
     import os
     import sys
     import site
+    from pathlib import Path
 
+    DJED_ROOT = Path(os.getenv('DJED_ROOT'))
+
+    print('Djed: ', DJED_ROOT)
     try:
-        site.addsitedir(os.path.join(os.getenv('DJED_ROOT'), 'venv/python39/Lib/site-packages'))
-        sys.path.append(os.path.join(os.getenv('DJED_ROOT'), 'src/dcc/maya/hooks'))
+        site.addsitedir(DJED_ROOT.joinpath('venv/python39/Lib/site-packages').as_posix())
+        sys.path.append(DJED_ROOT.joinpath('src').as_posix())
+        sys.path.append(DJED_ROOT.joinpath('src/dcc/maya/hooks').as_posix())
 
+        print('start DJED')
         import shelf
         shelf.main()
-        print('start DJED')
+        open_commandPort()
+
 
     except:
         print(traceback.format_exc())
+
+cmds.evalDeferred("init_djed()")
+# <<<<<<<<<
