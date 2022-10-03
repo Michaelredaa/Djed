6d1636ce18aef397cbd634a5f77b1c847aff4c56
michael <eng.michaelreda@gmail.com>
2022-10-03 16:40:18 +0200

    add instance module

---
+++ src/dcc/linker/instance.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Documentation:
+"""
+import os
+import site
+from pathlib import Path
+
+DJED_ROOT = Path(os.getenv('DJED_ROOT'))
+
+site.addsitedir(DJED_ROOT.joinpath('venv/python39/Lib/site-packages').as_posix())
+
+import pyblish.api
+
+def create_instance(data):
+    context = pyblish.api.Context()
+    instance = context.create_instance(**data)
+    return instance
+
+if __name__ == '__main__':
+    print(__name__)
