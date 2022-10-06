1dbf3596f01b3497e39cce24da571f36e6ccef25
Michael Reda <eng.michaelreda@gmail.com>
2022-10-06 13:48:53 +0200

    add file

---
+++ src/dcc/clarisse/api/remote_connect.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Documentation: 
+"""
+
+
+# ---------------------------------
+# Import Libraries
+import os
+import sys
+from pathlib import Path
+
+DJED_ROOT = Path(os.getenv("DJED_ROOT"))
+sysPaths = [DJED_ROOT.as_posix(), DJED_ROOT.joinpath('src').as_posix()]
+
+for sysPath in sysPaths:
+    if sysPath not in sys.path:
+        sys.path.append(sysPath)
+
+
+from utils.file_manager import FileManager
+from utils import clarisse_net as ix
+# ---------------------------------
+# Variables
+
+
+fm = FileManager()
+# ---------------------------------
+# Start Here
+def set_port_num(port_num=None):
+    if port_num is None:
+        port_num = fm.get_user_json("clarisse", "command_port")
+    return port_num
+
+
+def connect(ip='localhost', port_num=None):
+    try:
+        port_num = set_port_num(port_num)
+        port = ix.ClarisseNet(ip, port_num)
+        return port
+    except Exception as e:
+        print(e)
+        pass
+    return
+
+
+# Main Function
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
