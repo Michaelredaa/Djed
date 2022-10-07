fe90922fbb69fc28534873b500aa2666bbd6a9b2
Michael Reda <eng.michaelreda@gmail.com>
2022-10-07 10:27:32 +0200

    add __all__ variable

--- src/dcc/linker/__init__.py
+++ src/dcc/linker/__init__.py
@@ -3,5 +3,21 @@
 Documentation:
 """
 
+from .instance import create_instance
+from .to_clarisse import send_to_clarisse
+from .to_maya import send_to_maya
+from .to_spp import send_to_spp
+
+
+__all__ = [
+    'create_instance',
+    'send_to_clarisse',
+    'send_to_maya',
+    'send_to_spp',
+]
+
+
+
+
 if __name__ == '__main__':
     print(__name__)
