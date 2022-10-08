ad91c112032599e110711bba2add29cb2b169282
Michael Reda <eng.michaelreda@gmail.com>
2022-10-08 11:50:21 +0200

    add __init__

---
+++ src/dcc/clarisse/hooks/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Documentation: 
+"""
+
+
+# ---------------------------------
+# Import Libraries
+
+
+# ---------------------------------
+# Variables
+
+
+# ---------------------------------
+# Start Here
+
+
+# Main Function
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
