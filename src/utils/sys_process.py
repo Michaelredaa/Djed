a9925733bc0d71efea70a36ca1daacf227f21bfd
Michael Reda <eng.michaelreda@gmail.com>
2022-10-08 23:02:14 +0200

    add create_shortcut function

--- src/utils/sys_process.py
+++ src/utils/sys_process.py
@@ -31,3 +31,26 @@ def is_process_running(process_name):
 def execute_commmand(*args):
     subprocess.Popen(args)
 
+
+def create_shortcut(shortcut_path, target, arguments='', working_dir=''):
+    shortcut_path = Path(shortcut_path)
+    shortcut_path.parent.mkdir(parents=True, exist_ok=True)
+
+    def escape_path(path):
+        return str(path).replace('\\', '/')
+
+    js_content = f'''
+        var sh = WScript.CreateObject("WScript.Shell");
+        var shortcut = sh.CreateShortcut("{escape_path(shortcut_path)}");
+        shortcut.TargetPath = "{escape_path(target)}";
+        shortcut.Arguments = "{arguments}";
+        shortcut.WorkingDirectory = "{working_dir}";
+        shortcut.Save();'''
+
+    fd, path = tempfile.mkstemp('.js')
+    try:
+        with os.fdopen(fd, 'w') as f:
+            f.write(js_content)
+        subprocess.run([R'wscript.exe', path])
+    finally:
+        os.unlink(path)
