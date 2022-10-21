9332a563f6edb032fd3a84ad9c879401efeb10e7
Michael Reda <eng.michaelreda@gmail.com>
2022-10-21 17:45:57 +0200

    change version formate

--- src/version.py
+++ src/version.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Package that declaring Djed version.
+"""
 VERSION_MAJOR = 0
 VERSION_MINOR = 1
 VERSION_PATCH = 1
 
 
 version_info = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
-version = '%i.%i.%i' % version_info
+version = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
 __version__ = version
 
 __all__ = ['version', 'version_info', '__version__']
