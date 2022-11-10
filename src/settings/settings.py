926327f5f341d96124f8fa5656dfa363031a76ea
michael <eng.michaelreda@gmail.com>
2022-11-10 10:49:05 +0200

    add set_value fuction

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -40,16 +40,21 @@ def get_settings_data():
     return data.get('items', [])
 
 
+def set_settings_data(data_list):
+    data = {'items': data_list}
+    fm.write_json(user_settings_file, data)
+
+
 def error_on_reading(key, dict_name):
     message(None, 'Error', f'Corrupted settings file.\nCan not get "{key}" from "{dict_name}" dict settings')
 
 
 def get_dict(base_list, key):
-    my_dict = [x for x in base_list if x.get('name') == key]
-    if not my_dict:
+    dict_list = [x for x in base_list if x.get('name') == key]
+    if not dict_list:
         return False
     else:
-        return my_dict[0]
+        return dict_list[0]
 
 
 def get_value(key, *args):
@@ -61,7 +66,7 @@ def get_value(key, *args):
 
         if not my_dict:
             error_on_reading(key, arg)
-            return ''
+            return {}
 
         items_list = my_dict.get('children', [])
         for i, item in enumerate(items_list):
@@ -84,6 +89,22 @@ def get_value(key, *args):
     return my_dict
 
 
+def set_value(value, *args):
+    items_list = get_settings_data()
+
+    my_dict = {}
+    items_list_copy = items_list
+    for arg in args:
+        my_dict = get_dict(items_list_copy, arg)
+
+        items_list_copy = my_dict.get('children', [])
+
+    if not my_dict:
+        return
+    my_dict['value'] = value
+    set_settings_data(items_list)
+
+
 def get_colorspace_settings(key='aces_color_hdr'):
     value_dict = get_value(key, 'general', 'textures', 'colorspace', key)
     return value_dict.get('value', '')
@@ -97,26 +118,31 @@ def get_textures_settings(key='extensions'):
 def get_textures_patterns():
     value_dict = get_value('patterns', 'general', 'textures', 'patterns')
     patterns = {x['name']: x['value'] for x in value_dict.get('children', [])}
+    patterns.pop('extensions')
+    patterns.pop('hdr_extension')
     return patterns
 
 
 def get_dcc_cfg(*args):
     value_dict = get_value(args[-1], *args)
-
     if 'data' in value_dict:
         values = {x['name']: x.get('value') for x in value_dict.get('data', [])}
+        return values
+    elif 'children' in value_dict:
+        values = {x['name']: x.get('value') for x in value_dict.get('children', [])}
         return values
     else:
         return value_dict.get('value', '')
 
-def material_conversion(from_host, from_renderer, to_host, to_renderer, node='standard_surface'):
 
-    from_plugs = get_dcc_cfg(from_host, 'renderers', from_renderer, node+'_plugs')
-    to_plugs = get_dcc_cfg(to_host, 'renderers', to_renderer, node+'_plugs')
+def material_conversion(from_host, from_renderer, to_host, to_renderer, node='standard_surface'):
+    from_plugs = get_dcc_cfg(from_host, 'renderers', from_renderer, node + '_plugs')
+    to_plugs = get_dcc_cfg(to_host, 'renderers', to_renderer, node + '_plugs')
 
     plugs = {from_plugs[i]['name']: to_plugs[j] for i, j in zip(from_plugs, to_plugs)}
 
     return plugs
+
 
 def get_shading_node(host, renderer):
     render_dict = get_value(renderer, host, 'renderers', renderer)
@@ -124,15 +150,11 @@ def get_shading_node(host, renderer):
     return shading_nodes
 
 
-
 def shading_nodes_conversion(from_host, from_renderer, to_host, to_renderer):
     from_nodes = get_shading_node(from_host, from_renderer)
-    to_nodes = get_shading_node( to_host, to_renderer)
+    to_nodes = get_shading_node(to_host, to_renderer)
     nodes = {from_nodes[i]: to_nodes[j] for i, j in zip(from_nodes, to_nodes)}
     return nodes
-
-
-
 
 
 if __name__ == '__main__':
@@ -144,9 +166,8 @@ if __name__ == '__main__':
 
     # print(get_dcc_cfg('maya', 'renderers', 'arnold'))
     # print(get_value('arnold', 'maya', 'renderers', 'arnold'))
-    print(get_shading_node('maya', 'arnold'))
-    shading_nodes_conversion('clarisse', 'autodesk_standard_surface', 'maya', 'arnold')
-
+    # print(get_dcc_cfg("substance_painter", "texture_export"))
+    set_value('512', "maya", "plugins", "maya_substance_painter", "default_texture_resolution")
     sys.exit(app.exec_())
 
     print(__name__)
