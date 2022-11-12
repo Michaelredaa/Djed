a58183883ca1cf0ec0ba662c641078b6e9a3eb37
Michael Reda <eng.michaelreda@gmail.com>
2022-11-12 13:36:02 +0200

    enhance the selection of all nodes

--- src/dcc/maya/plugins/validate_materials.py
+++ src/dcc/maya/plugins/validate_materials.py
@@ -6,7 +6,7 @@ from maya import cmds
 
 
 class SelectInvalidMTLNodes(pyblish.api.Action):
-    label = "Select node"
+    label = "Select nodes"
     on = "failed"
     icon = "hand-o-up"
 
@@ -15,8 +15,22 @@ class SelectInvalidMTLNodes(pyblish.api.Action):
         for result in context.data["results"]:
             if result["error"]:
                 instance = result['instance']
-                shape_node = instance.data.get("output", {}).get('node')
-                cmds.select(shape_node)
+                invalid_nodes = instance.data.get("output", {}).get('invalid_nodes', [])
+                cmds.select([x.get('node') for x in invalid_nodes if x])
+
+
+class SelectInvalidMultiMTLNodes(pyblish.api.Action):
+    label = "Select nodes"
+    on = "failed"
+    icon = "hand-o-up"
+
+    def process(self, context, plugin):
+
+        for result in context.data["results"]:
+            if result["error"]:
+                instance = result['instance']
+                invalid_nodes = instance.data.get("output", {}).get('invalid_nodes', [])
+                cmds.select([x.get('node') for x in invalid_nodes if x])
 
 
 class SplitPerMaterials(pyblish.api.Action):
@@ -34,8 +48,9 @@ class SplitPerMaterials(pyblish.api.Action):
                 self.log.info("Split object by materials")
 
                 instance = result['instance']
-                shape_node = instance.data.get("output", {}).get('node')
-                ma.split_by_material(shape_node, keep_original=1)
+                invalid_nodes = instance.data.get("output", {}).get('invalid_nodes', [])
+                for item in invalid_nodes:
+                    ma.split_by_material(item.get('node'), keep_original=1)
 
 
 class ValidateShadingGroups(pyblish.api.InstancePlugin):
@@ -50,30 +65,68 @@ class ValidateShadingGroups(pyblish.api.InstancePlugin):
     hosts = ["maya"]
     families = ["model"]
     active = True
-    actions = [SelectInvalidMTLNodes, SplitPerMaterials]
+    actions = [SelectInvalidMTLNodes]
 
     def process(self, instance):
         self.log.info("Initialize validation binding materials")
 
         ma = Maya()
-        for shape_node in instance:
-            shape_name = shape_node.rsplit('|', 1)[-1]
+        no_shading_grps = []
+        lambert_shading_grps = []
 
+        for shape_node in instance:
             sgs = ma.list_all_DG_nodes(shape_node)
 
-            msg = ''
             if len(sgs) == 0:
-                instance.set_data("output", {'node': shape_node, 'sgs': sgs})
-                msg = f"'{shape_name}' have no shading group."
-
-            elif len(sgs) != 1:
-                instance.set_data("output", {'node': shape_node, 'sgs': sgs})
-                msg = f"'{shape_name}' have multiple material assign on one object. sgs: {sgs}"
+                invalid_nodes = {'node': shape_node, 'sgs': sgs}
+                no_shading_grps.append(invalid_nodes)
 
             elif sgs[0] == "initialShadingGroup":
-                instance.set_data("output", {'node': shape_node, 'sgs': sgs})
-                msg = f"'{shape_name}' have 'lambert1' material assign. ('lambert1' not allowed)"
+                invalid_nodes = {'node': shape_node, 'sgs': sgs}
+                lambert_shading_grps.append(invalid_nodes)
 
-            if msg:
-                self.log.error(msg)
-                raise pyblish.api.ValidationError(msg)
+        if no_shading_grps:
+            instance.set_data("output", {'invalid_nodes': no_shading_grps})
+            msg = f"Some nodes have no shading group '{no_shading_grps}'"
+            self.log.error(msg)
+            raise pyblish.api.ValidationError(msg)
+
+        if lambert_shading_grps:
+            instance.set_data("output", {'invalid_nodes': lambert_shading_grps})
+            msg = f"Some nodes have 'lambert1' material assign. ('lambert1' not allowed)'{lambert_shading_grps}'"
+            self.log.error(msg)
+            raise pyblish.api.ValidationError(msg)
+
+
+class ValidateMultipleMaterialAssign(pyblish.api.InstancePlugin):
+    """
+    To validate that one object assigned on one geometry
+    """
+
+    order = pyblish.api.ValidatorOrder + 0.21
+
+    optional = True
+    label = "Validate multi material assign"
+    hosts = ["maya"]
+    families = ["model"]
+    active = True
+    actions = [SelectInvalidMultiMTLNodes, SplitPerMaterials]
+
+    def process(self, instance):
+        self.log.info("Initialize validation binding materials")
+
+        ma = Maya()
+        faces_shading_grps = []
+
+        for shape_node in instance:
+            sgs = ma.list_all_DG_nodes(shape_node)
+
+            if len(sgs) != 1:
+                invalid_nodes = {'node': shape_node, 'sgs': sgs}
+                faces_shading_grps.append(invalid_nodes)
+
+        if faces_shading_grps:
+            instance.set_data("output", {'invalid_nodes': faces_shading_grps})
+            msg = f"Some nodes have multiple material assign on one object.'{faces_shading_grps}'"
+            self.log.error(msg)
+            raise pyblish.api.ValidationError(msg)
