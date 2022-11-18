3c763a31dcf17148295ce250b756dbbc4e779cd2
michael <eng.michaelreda@gmail.com>
2022-11-18 17:04:14 +0200

    add start integration

---
+++ src/dcc/maya/hooks/start.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Documentation:
+"""
+import os
+from pathlib import Path
+
+DJED_ROOT = Path(os.getenv('DJED_ROOT'))
+
+
+def add_maya_module(maya_module_path=None):
+    try:
+        if maya_module_path is None:
+            maya_module_path = Path.home().joinpath("Documents", "maya", "modules")
+        else:
+            maya_module_path = Path(str(maya_module_path))
+
+        maya_module_path.mkdir(parents=True, exist_ok=True)
+        mod_file = maya_module_path.joinpath("djed.mod")
+        root_path = DJED_ROOT.joinpath('src/dcc/maya/hooks/scripts').as_posix()
+        cmd = f'+ Djed 1.0 {root_path}\nscripts: {root_path}'
+
+        mod_file.unlink(missing_ok=True)
+
+        with mod_file.open('w') as f:
+            f.write(cmd)
+
+        return True
+    except Exception as e:
+        return str(e)
+
+
+if __name__ == '__main__':
+    print(__name__)
---
+++ src/dcc/spp/hooks/start.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Documentation:
+"""
+import os
+import sys
+import shutil
+from pathlib import Path
+
+DJED_ROOT = Path(os.getenv('DJED_ROOT'))
+sysPaths = [DJED_ROOT.as_posix(), DJED_ROOT.joinpath('src').as_posix()]
+for sysPath in sysPaths:
+    if sysPath not in sys.path:
+        sys.path.append(sysPath)
+
+from utils.sys_process import create_shortcut
+from settings.settings import get_dcc_cfg
+
+
+def create_spp_shortcut():
+    spp_exe = get_dcc_cfg("substance_painter", "configuration", "executable")
+    create_shortcut(
+        f'{os.getenv("PROGRAMDATA")}/Microsoft/Windows/Start Menu/Programs/Djed Adobe Substance 3D Painter.lnk',
+        spp_exe,
+        '--enable-remote-scripting',
+        spp_exe,
+    )
+
+
+def add_spp_startup():
+    source_file = DJED_ROOT.joinpath('src/dcc/spp/hooks/djed_startup.py')
+    dist_file = Path.home().joinpath("Documents/Adobe/Adobe Substance 3D Painter/python/startup/djed_startup.py")
+    if dist_file.is_file():
+        try:
+            dist_file.unlink(missing_ok=True)
+        except:
+            pass
+
+    if not dist_file.parent.is_dir():
+        dist_file.parent.mkdir(parents=True, exist_ok=True)
+
+    try:
+        shutil.copy(source_file, dist_file)
+        return True
+    except:
+        return 'Unable to add integration to substance painter'
+
+
+if __name__ == '__main__':
+    print(__name__)
