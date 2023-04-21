4c92b95649fdb2d396f4b2019c6138551a953fe8
Michael Reda <eng.michaelreda@gmail.com>
2023-04-21 15:22:26 +0200

    add test for TestPathResolver

--- djed/tests/test_file_manager.py
+++ djed/tests/test_file_manager.py
@@ -3,7 +3,6 @@
 Documentation: 
 """
 
-
 # ---------------------------------
 # Import Libraries
 import sys
@@ -11,7 +10,6 @@ import os
 import platform
 
 import unittest
-
 
 DJED_ROOT = os.getenv('DJED_ROOT')
 utils_path = os.path.join(DJED_ROOT, 'djed')
@@ -21,22 +19,21 @@ for sysPath in sysPaths:
     if sysPath not in sys.path:
         sys.path.append(sysPath)
 
-from djed.utils.file_manager import FileManager
+from djed.utils.file_manager import FileManager, PathResolver
 
 # ---------------------------------
 # Variables
 fm = FileManager()
+
 
 # ---------------------------------
 # Start Here
 class TestFileManager(unittest.TestCase):
 
     def test_user_documents(self):
-
         user_document_dir = fm.user_documents.as_posix()
 
         if platform.system() == 'Windows':
-
             self.assertEqual(
                 user_document_dir,
                 f"C:/Users/{os.environ['USERNAME']}/Documents/Djed"
@@ -54,6 +51,15 @@ class TestFileManager(unittest.TestCase):
     def test_user_json(self):
         fm.set_user_json(foo='bar')
         self.assertEqual(fm.get_user_json('foo'), 'bar')
+
+class TestPathResolver(unittest.TestCase):
+
+    def test_resolve_path(self):
+        resolver = PathResolver(r"{foo}/to/{bar}/{name}.{ext}")
+
+        resolved_path = resolver.format(foo="/foo_value", bar="path/to/bar", name="asset", ext="abc")
+
+        self.assertEqual(resolved_path, "/foo_value/to/path/to/bar/asset.abc")
 
 
 
